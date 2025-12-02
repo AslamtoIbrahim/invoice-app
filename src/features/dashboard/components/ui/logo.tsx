bf8875f9ea1c53fc7b9b14556d18111c7f8c61e7
AslamtoIbrahim <aslamtoibrahim@gmail.com>
2025-12-02 15:10:04 +0100

    change: img path

--- src/features/dashboard/components/ui/logo.tsx
+++ src/features/dashboard/components/ui/logo.tsx
@@ -5,7 +5,7 @@ function Logo() {
         className="absolute bottom-0 left-0 z-10 h-1/2 w-full rounded-tl-2xl rounded-br-2xl
       bg-violet-400"
       />
-      <img className="relative z-50 " src="/public/logo.svg" alt="" />
+      <img className="relative z-50 " src="/logo.svg" alt="" />
     </div>
   );
 }
